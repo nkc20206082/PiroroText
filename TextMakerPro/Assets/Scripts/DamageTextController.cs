9b98dd47efe9a1fec1b01d83515820d003dbaf2d
DESKTOP-E8EHEE1\user <nkc20206082@denpa.jp>
2022-03-25 20:55:13 +0300

    Commit

--- TextMakerPro/Assets/Scripts/DamageTextController.cs
+++ TextMakerPro/Assets/Scripts/DamageTextController.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class DamageTextController : MonoBehaviour
 {
+    private const float _Duration=0.6f;
     private TextMeshPro _TextMeshPro;
     private void Awake()
     {
@@ -12,8 +13,8 @@ public class DamageTextController : MonoBehaviour
     //テキストの動き
     void OnEnable()
     {
-        DOTween.ToAlpha(() => _TextMeshPro.color, a => _TextMeshPro.color = a, 0.0f, 0.6f).OnComplete(() => { _TextMeshPro.color = Color.white; }) ;
-        this.transform.DOMove(new Vector2(transform.position.x, transform.position.y+1f), 0.6f)
+        DOTween.ToAlpha(() => _TextMeshPro.color, a => _TextMeshPro.color = a, 0.0f, _Duration).OnComplete(() => { _TextMeshPro.color = Color.white; }) ;
+        this.transform.DOMove(new Vector2(transform.position.x, transform.position.y+1f), _Duration)
                             .OnComplete(() => {gameObject.SetActive(false); });
     }
 }
